b35ba9312a0b448e551cbbdefda6e545d3bac65e
Vadym <107150489+Vadim0036@users.noreply.github.com>
2023-04-14 11:37:04 -0500

    Update TypyingProgram.java

--- TypyingProgram.java
+++ TypyingProgram.java
@@ -1,36 +1,34 @@
-
-public class TypyingProgram 
+public class TypingProgram 
 {
 	private Text typyingText;
-	private TypyingWindow window;
+	private TypingWindow window;
 	
-	private char current_char; 
 	private int current_index; 
 	
 	
-	public TypyingProgram()
+	public TypingProgram()
 	{
 		this.typyingText = new Text();
-		this.window = new TypyingWindow(this);
+		this.window = new TypingWindow( this );
 		
 		this.current_index = 0;
-		this.current_char = this.typyingText.getTypyingText().charAt(this.current_index);
 	}
-	
 	
 	public void runProgram()
 	{
 		this.window.runProgram();
+		this.window.setText(this.typyingText);
 	}
-	
 	
 	public boolean checkInput(char given_char)
 	{
+		char current_char = this.typyingText.getTypyingTextCharAt(this.current_index);
 		if(current_char == given_char)
 		{
+			System.out.println( "CORRECT" );
 			this.current_index ++;
-			this.current_char = this.typyingText.getTypyingText().charAt(this.current_index);
 			this.typyingText.addChar(given_char);
+			this.window.setText( this.typyingText );
 			return true;
 		}
 		else 
