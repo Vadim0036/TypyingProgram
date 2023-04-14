da375850ccf93c84f43c6a1d9baef7a9a04b2a54
Vadym <107150489+Vadim0036@users.noreply.github.com>
2023-04-13 22:50:35 -0500

    Update TypyingProgram.java

--- TypyingProgram.java
+++ TypyingProgram.java
@@ -1,50 +1,18 @@
-public class TypingProgram 
+
+public class TypyingProgram 
 {
 	private char current_char; 
 	private int current_index; 
-	private Text text;
-	
-	private TypingWindow window;
+	private TypyingWindow window;
 	
 	
-	public TypingProgram()
+	public TypyingProgram()
 	{
-		this.window = new TypingWindow( this );
-		this.text = new Text();
-		
-		this.current_index = 0; 
-		this.current_char = text.getString().charAt(current_index);
+		this.window = new TypyingWindow(this);
 	}
 	
 	public void runProgram()
 	{
-		this.window.setUpTextPanel(this.text);
 		this.window.runProgram();
 	}
-	
-	public void checkInput(char given_char)
-	{
-		if(this.current_char == given_char)
-		{
-			this.window.changeColor( this.current_index, this.text );
-			this.current_index ++;
-			this.current_char = text.getString().charAt(current_index); 
-			System.out.println("Correct");
-		}
-		else {
-			System.out.println("Wrong");
-		}
-		if(isEnd()) 
-		{
-			this.window.showResult( 0, 0 );
-			System.out.println("Game is over ");	
-		}
-	}
-	
-	private boolean isEnd()
-	{
-		if (this.current_index >= this.text.getNumberOfChar()) return true;
-		else return false;
-	}	
 }
-
