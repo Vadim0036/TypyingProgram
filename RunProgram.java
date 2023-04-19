f38f39cbb7b4b29b1c834385ffcd1a218f7dc815
Vadym <107150489+Vadim0036@users.noreply.github.com>
2023-04-19 10:39:34 -0500

    Update RunProgram.java

--- RunProgram.java
+++ RunProgram.java
@@ -1,9 +1,15 @@
 
-public class RunProgram 
-{
+public class RunProgram {
+	
 	public static void main(String[] args)
-	{
+	{   
 		TypingProgram myProgram = new TypingProgram();
-		myProgram.runProgram();
-	}
+		myProgram.runProgram();	
+	}	
 }
+
+/*
+		List what to do: 
+		3.) Set borders for text
+		4.) Add more text 
+*/
